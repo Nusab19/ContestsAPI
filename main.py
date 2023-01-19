17d212118235221a29c461bd2c68a93c7b335bcf
= <Nusabtaha33@gmail.com>
2023-01-19 18:23:41 +0600

    Caching data for faster response

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from fastapi import FastAPI
 
+from fastapi import FastAPI
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
+
+# Local Helpers
+from assets import cache
 from platforms import atcoder, codeforces, hackerearth
 
 import httpx
@@ -11,6 +16,41 @@ HTTPX_CLIENT = httpx.AsyncClient(timeout=300)
 
 
 app = FastAPI()
+
+scheduler = AsyncIOScheduler()
+
+cachedData = {}
+
+
+keyword_platforms = {
+    "1": "atcoder",
+    "2": "codeforces",
+    "3": "hackerearth"
+}
+
+
+
+platform_funcs = {
+"1":atcoder.getContests,
+"2":codeforces.getContests,
+"3":hackerearth.getContests
+}
+
+
+@app.on_event("startup")
+async def fx():
+    await cacheOnStart()
+    scheduler.add_job(cacheOnStart, 'interval', seconds=7*60)
+    scheduler.start()
+
+
+async def cacheOnStart():
+    cachedData["atcoder"] = await atcoder.getContests(HTTPX_CLIENT)
+
+    cachedData["codeforces"] = await codeforces.getContests(HTTPX_CLIENT)
+
+    cachedData["hackerearth"] = await hackerearth.getContests(HTTPX_CLIENT)
+    print("Cached")
 
 
 platforms = "atcoder codeforces hackerearth".split()
@@ -44,6 +84,7 @@ async def allPlatformContests():
     data = {"ok": True}
     try:
         data["atcoder"] = await atcoder.getContests(ses)
+
     except BaseException:
         data["ok"] = False
 
@@ -57,15 +98,23 @@ async def allPlatformContests():
     except BaseException:
         data["ok"] = False
 
-    return data
+    try:
+        return data
+    finally:
+        del data["ok"]
+        cachedData.update(data)
 
 
 @app.get("/atcoder")
 @app.get("/1")
 async def atcoderContests():
-    ses = HTTPX_CLIENT
     try:
-        return await atcoder.getContests(ses)
+        data = await atcoder.getContests(HTTPX_CLIENT)
+        try:
+            return data
+        finally:
+            cachedData["atcoder"] = data
+
     except Exception as e:
         return {
             "ok": False,
@@ -76,9 +125,13 @@ async def atcoderContests():
 @app.get("/codeforces")
 @app.get("/2")
 async def codeforcesContests():
-    ses = HTTPX_CLIENT
     try:
-        return await codeforces.getContests(ses)
+        data = await codeforces.getContests(HTTPX_CLIENT)
+        try:
+            return data
+        finally:
+            cachedData["codeforces"] = data
+
     except Exception as e:
         return {
             "ok": False,
@@ -89,14 +142,56 @@ async def codeforcesContests():
 @app.get("/hackerearth")
 @app.get("/3")
 async def hackerEarthContests():
-    ses = HTTPX_CLIENT
     try:
-        return await hackerearth.getContests(ses)
+        data = await hackerearth.getContests(HTTPX_CLIENT)
+        try:
+            return data
+        finally:
+            cachedData["hackerearth"] = data
+
     except Exception as e:
         return {
             "ok": False,
             "message": "Failed to fetch contests",
             "error": str(e)}
+
+
+@app.get("/cached/all")
+async def all_cached():
+    cachedData.update({"ok": True})
+    return cachedData
+
+
+
+
+
+@app.get("/cached/{platform}")
+async def cached_result(platform: str):
+    if platform in keyword_platforms:
+        platform = keyword_platforms.get(platform)
+    
+    if platform not in keyword_platforms.values():
+        return {"ok":False,
+        "message":f"`{platform}` is not in the available platform list.\nCheck the spelling or visit github.com/Nusab19/ContestsAPI"
+        }
+    
+    
+    
+    platform_id = {j:i for (i,j) in keyword_platforms.items()}.get(platform)
+    
+    func = platform_funcs.get(platform_id)
+    
+    try:
+        data = await func(HTTPX_CLIENT)
+        cachedData[platform] = data
+        return data
+    except Exception as e:
+        return {
+            "ok": False,
+            "message": "Failed to fetch contests",
+            "error": str(e)}
+    
+    
 
 
 if __name__ == "__main__":
